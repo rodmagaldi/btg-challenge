cae26ea576549a5bac1d4cb4ca8a8b0e0176a304
Rodrigo Magaldi <rod.magaldi@gmail.com>
2024-01-09 14:37:58 -0300

    chore: update and simplify test suite

--- tests/test_main.py
+++ tests/test_main.py
@@ -18,294 +18,61 @@ class TestGame:
     def test_invalid_inputs(self):
         response = self.client.post(
             '/',
-            json={"player_one_choice": "invalido 1", "player_two_choice": "invalido 2"}
+            json={"player_one_choice": "invalid 1", "player_two_choice": "invalid 2"}
         )
         assert response.status_code == 400
         data = response.get_json()
         assert len(data.get("errors")) == 2
 
-    def test_ties(self):
+    @pytest.mark.parametrize("player_one_choice, player_two_choice, message, winner", [
+        ("rock", "scissors", "Pedra esmaga tesoura.", "rock"),
+        ("scissors", "rock", "Pedra esmaga tesoura.", "rock"),
+        ("rock", "lizard", "Pedra esmaga lagarto.", "rock"),
+        ("lizard", "rock", "Pedra esmaga lagarto.", "rock"),
+        ("paper", "rock", "Papel cobre pedra.", "paper"),
+        ("rock", "paper", "Papel cobre pedra.", "paper"),
+        ("paper", "spock", "Papel refuta Spock.", "paper"),
+        ("spock", "paper", "Papel refuta Spock.", "paper"),
+        ("scissors", "paper", "Tesoura corta papel.", "scissors"),
+        ("paper", "scissors", "Tesoura corta papel.", "scissors"),
+        ("scissors", "lizard", "Tesoura decapita lagarto.", "scissors"),
+        ("lizard", "scissors", "Tesoura decapita lagarto.", "scissors"),
+        ("lizard", "spock", "Lagarto envenena Spock.", "lizard"),
+        ("spock", "lizard", "Lagarto envenena Spock.", "lizard"),
+        ("lizard", "paper", "Lagarto come papel.", "lizard"),
+        ("paper", "lizard", "Lagarto come papel.", "lizard"),
+        ("spock", "scissors", "Spock quebra tesoura.", "spock"),
+        ("scissors", "spock", "Spock quebra tesoura.", "spock"),
+        ("spock", "rock", "Spock vaporiza pedra.", "spock"),
+        ("rock", "spock", "Spock vaporiza pedra.", "spock"),
+    ])
+    def test_outcome(self, player_one_choice, player_two_choice, message, winner):
+        """
+        Tests all possibble outcomes for players choosing different plays.
+        """
         response = self.client.post(
             '/',
-            json={"player_one_choice": "rock", "player_two_choice": "rock"}
+            json={"player_one_choice": player_one_choice, "player_two_choice": player_two_choice}
+        )
+        assert response.status_code == 200
+        data = response.get_json()
+        assert data.get("message") == message
+        assert data.get("player_one_choice") == player_one_choice
+        assert data.get("player_two_choice") == player_two_choice
+        assert data.get("winner") == winner
+
+    @pytest.mark.parametrize("players_choice", ["rock", "paper", "scissors", "lizard", "spock"])
+    def test_ties(self, players_choice):
+        """
+        Tests all possibble outcomes for players choosing the same play.
+        """
+        response = self.client.post(
+            '/',
+            json={"player_one_choice": players_choice, "player_two_choice": players_choice}
         )
         assert response.status_code == 200
         data = response.get_json()
         assert data.get("message") == "Empate!"
-        assert data.get("player_one_choice") == "rock"
-        assert data.get("player_two_choice") == "rock"
+        assert data.get("player_one_choice") == players_choice
+        assert data.get("player_two_choice") == players_choice
         assert data.get("winner") == None
-        
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "paper", "player_two_choice": "paper"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Empate!"
-        assert data.get("player_one_choice") == "paper"
-        assert data.get("player_two_choice") == "paper"
-        assert data.get("winner") == None
-        
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "scissors", "player_two_choice": "scissors"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Empate!"
-        assert data.get("player_one_choice") == "scissors"
-        assert data.get("player_two_choice") == "scissors"
-        assert data.get("winner") == None
-        
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "lizard", "player_two_choice": "lizard"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Empate!"
-        assert data.get("player_one_choice") == "lizard"
-        assert data.get("player_two_choice") == "lizard"
-        assert data.get("winner") == None
-        
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "spock", "player_two_choice": "spock"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Empate!"
-        assert data.get("player_one_choice") == "spock"
-        assert data.get("player_two_choice") == "spock"
-        assert data.get("winner") == None
-
-    def test_rock_beats_scissors(self):
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "rock", "player_two_choice": "scissors"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Pedra esmaga tesoura."
-        assert data.get("player_one_choice") == "rock"
-        assert data.get("player_two_choice") == "scissors"
-        assert data.get("winner") == "rock"
-        
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "scissors", "player_two_choice": "rock"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Pedra esmaga tesoura."
-        assert data.get("player_one_choice") == "scissors"
-        assert data.get("player_two_choice") == "rock"
-        assert data.get("winner") == "rock"
-
-    def test_rock_beats_lizard(self):
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "rock", "player_two_choice": "lizard"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Pedra esmaga lagarto."
-        assert data.get("player_one_choice") == "rock"
-        assert data.get("player_two_choice") == "lizard"
-        assert data.get("winner") == "rock"
-        
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "lizard", "player_two_choice": "rock"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Pedra esmaga lagarto."
-        assert data.get("player_one_choice") == "lizard"
-        assert data.get("player_two_choice") == "rock"
-        assert data.get("winner") == "rock"
-
-    def test_paper_beats_rock(self):
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "paper", "player_two_choice": "rock"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Papel cobre pedra."
-        assert data.get("player_one_choice") == "paper"
-        assert data.get("player_two_choice") == "rock"
-        assert data.get("winner") == "paper"
-        
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "rock", "player_two_choice": "paper"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Papel cobre pedra."
-        assert data.get("player_one_choice") == "rock"
-        assert data.get("player_two_choice") == "paper"
-        assert data.get("winner") == "paper"
-
-    def test_paper_beats_spock(self):
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "paper", "player_two_choice": "spock"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Papel refuta Spock."
-        assert data.get("player_one_choice") == "paper"
-        assert data.get("player_two_choice") == "spock"
-        assert data.get("winner") == "paper"
-        
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "spock", "player_two_choice": "paper"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Papel refuta Spock."
-        assert data.get("player_one_choice") == "spock"
-        assert data.get("player_two_choice") == "paper"
-        assert data.get("winner") == "paper"
-
-    def test_scissors_beats_paper(self):
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "scissors", "player_two_choice": "paper"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Tesoura corta papel."
-        assert data.get("player_one_choice") == "scissors"
-        assert data.get("player_two_choice") == "paper"
-        assert data.get("winner") == "scissors"
-        
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "paper", "player_two_choice": "scissors"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Tesoura corta papel."
-        assert data.get("player_one_choice") == "paper"
-        assert data.get("player_two_choice") == "scissors"
-        assert data.get("winner") == "scissors"
-
-    def test_scissors_beats_lizard(self):
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "scissors", "player_two_choice": "lizard"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Tesoura decapita lagarto."
-        assert data.get("player_one_choice") == "scissors"
-        assert data.get("player_two_choice") == "lizard"
-        assert data.get("winner") == "scissors"
-        
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "lizard", "player_two_choice": "scissors"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Tesoura decapita lagarto."
-        assert data.get("player_one_choice") == "lizard"
-        assert data.get("player_two_choice") == "scissors"
-        assert data.get("winner") == "scissors"
-
-    def test_lizard_beats_spock(self):
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "lizard", "player_two_choice": "spock"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Lagarto envenena Spock."
-        assert data.get("player_one_choice") == "lizard"
-        assert data.get("player_two_choice") == "spock"
-        assert data.get("winner") == "lizard"
-        
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "spock", "player_two_choice": "lizard"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Lagarto envenena Spock."
-        assert data.get("player_one_choice") == "spock"
-        assert data.get("player_two_choice") == "lizard"
-        assert data.get("winner") == "lizard"
-
-    def test_lizard_beats_paper(self):
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "lizard", "player_two_choice": "paper"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Lagarto come papel."
-        assert data.get("player_one_choice") == "lizard"
-        assert data.get("player_two_choice") == "paper"
-        assert data.get("winner") == "lizard"
-        
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "paper", "player_two_choice": "lizard"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Lagarto come papel."
-        assert data.get("player_one_choice") == "paper"
-        assert data.get("player_two_choice") == "lizard"
-        assert data.get("winner") == "lizard"
-
-    def test_spock_beats_scissors(self):
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "spock", "player_two_choice": "scissors"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Spock quebra tesoura."
-        assert data.get("player_one_choice") == "spock"
-        assert data.get("player_two_choice") == "scissors"
-        assert data.get("winner") == "spock"
-        
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "scissors", "player_two_choice": "spock"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Spock quebra tesoura."
-        assert data.get("player_one_choice") == "scissors"
-        assert data.get("player_two_choice") == "spock"
-        assert data.get("winner") == "spock"
-
-    def test_spock_beats_rock(self):
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "spock", "player_two_choice": "rock"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Spock vaporiza pedra."
-        assert data.get("player_one_choice") == "spock"
-        assert data.get("player_two_choice") == "rock"
-        assert data.get("winner") == "spock"
-        
-        response = self.client.post(
-            '/',
-            json={"player_one_choice": "rock", "player_two_choice": "spock"}
-        )
-        assert response.status_code == 200
-        data = response.get_json()
-        assert data.get("message") == "Spock vaporiza pedra."
-        assert data.get("player_one_choice") == "rock"
-        assert data.get("player_two_choice") == "spock"
-        assert data.get("winner") == "spock"
